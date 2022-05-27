60313137fe7a53417f8609626418d1bc49542e5f
gundamss <chauyuhin1998@gmail.com>
2022-05-27 10:57:20 -0600

    first

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cse210_03
+{
+    class Program
+    {
+        //start the game
+        static void Main(string[] args)
+        {
+            Run_Terminal terminal = new Run_Terminal();
+            terminal.start_terminal();
+        }
+    }
+}
---
+++ game/guess_sequence.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace cse210_03
+{
+
+
+    public class Guessinggame
+    {
+        private Draw drawing_the_man = new Draw();
+        public void Startgame()
+        {
+                drawing_the_man.Board();
+
+        }
+
+    }
+}
